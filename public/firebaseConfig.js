9ac2ef3caff91639ba00c68c3a10829d0c675045
impacttps-hub <impacttps@gmail.com>
2026-04-06 16:49:32 +0100

    Integracion Git x Anti

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,5 +1,4 @@
 // Configuración de Firebase para Impact PS (impactps-75819)
-// POR FAVOR: Copia aquí los valores que faltan desde tu Consola de Firebase.
 
 const firebaseConfig = {
   apiKey: "AIzaSyC_fDTfF7XCXtbrjIoMhn0_FK2XZhSWTs8",
@@ -10,4 +9,5 @@ const firebaseConfig = {
   appId: "1:735325226428:web:1b869ea9c7aef7948317b9",
   measurementId: "G-FJVW20HC28"
 };
+
 export default firebaseConfig;
